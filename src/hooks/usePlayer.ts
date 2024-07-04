5af94ceb5eb97b7611648d84c6a77102114e10b9
black-RAM <pplanes311@gmail.com>
2024-07-04 14:01:01 +0300

    usePlayer hook mutes audio when tab is not open

--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,13 +1,33 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 
 const usePlayer = (src: string, loop = false) => {
   const audio = useMemo(() => new Audio(src), [src])
   audio.loop = loop
+
+  // Mute the audio when the tab is not open
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        audio.muted = true
+      } else {
+        audio.muted = false
+      }
+    }
+
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    }
+  }, [audio])
+
   const play = () => audio.play()
+  
   const pause = () => {
     audio.pause()
     audio.currentTime = 0
   }
+  
   return [play, pause]
 }
 
