b0734b6809048f3682f43c86cb879ea93379b10c
black-RAM <pplanes311@gmail.com>
2024-07-04 19:38:31 +0300

    tiles for data story instead of cards

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,6 +5,8 @@ import useObserver from "../hooks/useObserver"
 
 import woosh from "../assets/whoosh.mp3"
 import mallBackground from "../assets/mallBackground.jpg"
+import cuboids3DArt from "../assets/milad-fakurian-9waPFLIzs1E-unsplash.jpg"
+import squarePrisms3DArt from "../assets/milad-fakurian-drqGSDR-IUs-unsplash.jpg"
 import customer1 from "../assets/christian-buehner-DItYlc26zVI-unsplash(1).jpg"
 import customer2 from "../assets/christopher-campbell-rDEOVtE7vOs-unsplash(1).jpg"
 import customer3 from "../assets/petr-sevcovic-e12wQLAjQi0-unsplash(1).jpg"
@@ -14,16 +16,6 @@ import notification from "../assets/notification.mp3"
 import cameraSound from "../assets/cameraSound.mp3"
 import logo from "/kart.png"
 import "../styles/landing.css"
-
-interface Parent{
-  children: React.ReactNode
-}
-
-const CustomTopBorder: React.FC<Parent> = ({children}) => 
-  <article className="max-w-80 h-[30rem] mb-8 rounded bg-zinc-300 bg-opacity-75 overflow-hidden">
-    <div className="border-t-8 border-l-8 border-r-8 border-zinc-200 rounded h-52"></div>
-    <div className="mx-8 relative -top-44">{children}</div>
-  </article>
 
 interface TestimonialProps {
   imgUrl: string,
@@ -79,12 +71,11 @@ const LandingPage = () => {
     const {current} = testimonialsRef
     if(!current) return
     if(isVisible) {
-      const camera = () => {
+      if(testimonialSoundRef.current) clearInterval(testimonialSoundRef.current)
+      testimonialSoundRef.current = setInterval(() => {
         playCamera()
         setTimeout(endCamera, 1000)
-      }
-      camera()
-      testimonialSoundRef.current = setInterval(camera, 4000)
+      }, 4000)
       current.classList.add("animated")
     } else {
       if(testimonialSoundRef.current) clearInterval(testimonialSoundRef.current)
@@ -108,40 +99,24 @@ const LandingPage = () => {
         <div className="relative overflow-hidden flex items-end">
           <div id="welcome-container" className="bg-zinc-50 bg-opacity-80 rounded-tr w-3/4 p-4 backdrop-blur-sm">
             <h2 className="text-3xl w-max p-2 font-bold text-slate-900 bg-zinc-50 shadow">Welcome to GoKart</h2>
-            <p className="mt-2 text-lg font-semibold text-slate-800">Welcome to Go Kart – your ultimate destination for a shopping experience that’s as exhilarating as a high-speed race. Here, shopping isn't just an errand; it's an adventure. Imagine a place where every click and scroll is like taking a sharp turn on a thrilling race track, where the anticipation of finding that perfect item is as electrifying as the countdown before a race.</p>
+            <p className="mt-2 text-lg font-semibold text-slate-800">
+              Envision shopping as visceral as a 200 miles per hour race! Imagine the scent of fresh leather, the sound of engines roaring, and the sight of sleek, shiny products zooming into your cart. Our deals are as sweet as victory champagne, and our variety as wide as a racetrack. Ready to put the pedal to the metal? Buckle up, because at GoKart, we turn every shopping spree into a Grand Prix of savings and smiles!
+            </p>
           </div>
         </div>
       </header>
 
-      <section className="flex justify-center bg-fixed" id="floating-cards">
-        <div className="flex gap-x-8 p-8">
-          <div>
-            <CustomTopBorder>
-              <h2 className="text-xl underline font-medium mb-2">Navigate with Ease</h2>
-              <p className="text-gray-900">Step into Go Kart and feel the rush of excitement wash over you. Our virtual aisles are designed to mimic the sleek, streamlined tracks of a go-kart race, leading you effortlessly to your destination. Picture yourself weaving through a vibrant marketplace, each product a hidden gem waiting to be discovered. The interface is as intuitive and responsive as a finely tuned kart, responding to your every command with lightning speed.</p>
-            </CustomTopBorder>
-            
-            <CustomTopBorder>
-              <h2 className="text-xl underline font-medium mb-2">Engage Your Senses</h2>
-              <p>As you navigate our store, let your senses be your guide. The vivid images of our high-quality products almost leap off the screen, inviting you to reach out and touch them. Imagine the texture of a luxurious cashmere sweater, the crisp snap of a fresh apple, the gleam of a new kitchen gadget catching the light just so. You can almost hear the satisfying click of adding an item to your cart, each one a milestone on your shopping journey.</p>
-            </CustomTopBorder>
-          </div>
-          
-          <div className="mt-16">
-            <CustomTopBorder>
-              <h2 className="text-xl underline font-medium mb-2">Expert Assistance</h2>
-              <p>Our team at Go Kart is here to ensure your experience is as smooth and enjoyable as a victory lap. Our customer service experts are the pit crew of your shopping journey, always ready with a friendly wave and expert advice. They're here to help you fine-tune your shopping strategy, ensuring that you find exactly what you're looking for, and maybe even something unexpected that makes your heart race with joy.</p>
-            </CustomTopBorder>
+      <section className="h-96 bg-fixed bg-cover" style={{backgroundImage: `url(${cuboids3DArt})`}}></section>
 
-            <CustomTopBorder>
-              <h2 className="text-xl underline font-medium mb-2">Celebrate Your Wins</h2>
-              <p>Every purchase at Go Kart is a celebration, a confetti-filled moment of triumph. Imagine crossing the finish line, arms raised in victory, as your order confirmation pops up on the screen. Feel the thrill of knowing that your treasures are on their way to you, ready to enhance your life and bring you joy.</p>
-            </CustomTopBorder>
-          </div>
-        </div>
+      <section className="grid grid-rows-3 md:grid-cols-3">
+        <article className="row-span-2 col-span-2 bg-zinc-100 h-96"></article>
+        <article className="col-start-3 row-span-3 bg-stone-600"></article>
+        <article className="col-start-1 col-span-2 row-start-3 bg-slate-950"></article>
       </section>
+      
+      <section className="h-96 bg-fixed bg-cover" style={{backgroundImage: `url(${squarePrisms3DArt})`}}></section>
 
-      <section className="bg-gradient-to-b from-[#144077] via-cyan-800 to-emerald-700 py-4">
+      <section className="bg-gradient-to-b from-indigo-950 to-rose-950 py-4">
         <hgroup className="text-zinc-100 text-center my-4">
           <h3 className="font-light text-2xl">What Our Customers</h3>
           <h2 className="font-bold text-4xl">Are Saying</h2>
@@ -159,7 +134,7 @@ const LandingPage = () => {
         </div>
       </section>
 
-      <section ref={ctaRef} className="bg-gradient-to-br from-emerald-200 via-amber-200 to-red-200" id="cta">
+      <section ref={ctaRef} className="bg-gradient-to-br from-rose-100 via-orange-100 to-amber-100" id="cta">
         <div className="flex flex-col justify-around px-4 min-h-80 overflow-hidden" style={{backgroundImage: `url(${logo})`}}>
           <p className="text-bubble left">Ready to experience the thrill of seamless online shopping?</p>
           <p className="text-bubble right">Start your engines and shop at GoKart today for unbeatable speed, service, and quality!</p>
