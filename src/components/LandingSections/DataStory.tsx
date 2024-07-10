6e7749551ec007747cc2392416e3941e81c15de8
black-RAM <pplanes311@gmail.com>
2024-07-10 23:12:05 +0300

    slider to choropleth, total sales section of data story

--- src/components/LandingSections/DataStory.tsx
+++ src/components/LandingSections/DataStory.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import * as Plot from "@observablehq/plot"
 import * as d3 from "d3"
 import * as topojson from "topojson-client"
@@ -8,11 +8,10 @@ import usaTopology from "../../assets/counties-10m.json"
 import marketSharesCSV from "../../assets/broadband_long2000-2018rev.csv?raw"
 import useVW from "../../hooks/useVW"
 
-const DataStory = () => {
-  const vw = useVW()
-
-  // customer growth area graph
+const CustomerGrowth = () => {
   const customerGrowthGraphRef = useRef<HTMLElement>(null)
+
+  // Area graph using Observable Plot
   useEffect(() => {
     const customerGrowthDataTyped = customerGrowthData.map(({ year, customers, type }) => ({
       year: new Date(year, 0, 1),
@@ -34,7 +33,24 @@ const DataStory = () => {
     }
   }, [])
 
-  // product-category distribution pie chart
+  return (
+    <article className="row-span-2 col-span-2 bg-zinc-100 text-zinc-800 p-2">
+      <h2 className="text-5xl font-black uppercase">Here are the <b className="text-blue-800">facts</b></h2>
+      <div className="grid lg:grid-cols-[75%_25%] xl:grid-cols-[640px_1fr]">
+        <figure ref={customerGrowthGraphRef}></figure>
+        <div className="flex flex-col justify-center p-4">
+          <h3 className="text-lg capitalize underline">Unprecedented customer <b>growth</b></h3>
+          <p>Our customer base has surged from zero to 25,000 in just 20 years. Join thousands of satisfied shoppers and experience why GoKart is the go-to destination for all your needs.</p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+const CategoryDistribution = () => {
+  const vw = useVW()
+
+  // pie chart using D3
   useEffect(() => {
     const width = Math.max(vw * 30, 256)
     const height = width
@@ -102,13 +118,34 @@ const DataStory = () => {
     }
   }, [vw])
 
-  // choropleth
+  return (
+    <article className="col-start-3 row-span-3 p-4 bg-blue-950">
+      <h3 className="text-zinc-50 font-bold text-xl capitalize text-center">A wealth of product categories</h3>
+      <div className="flex flex-wrap justify-center gap-x-4">
+        <figure className="relative basis-[max(30vw,256px)] my-8">
+          <div id="category-pie-chart"></div>
+          <div className="absolute inset-0 grid justify-center items-center">
+            <caption id="category-pie-chart-legend" className="grid grid-cols-[min-content_1fr] gap-x-1 justify-center text-zinc-400 text-left"></caption>
+          </div>
+        </figure>
+        <div className="flex items-center basis-64 grow">
+          <p className="text-zinc-200 text-center">Understand the variety of products we offer by exploring the distribution of items across different categories. This breakdown highlights our diverse inventory, ensuring something for everyone.</p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+const MarketShareChoropleth = () => {
   const choroplethRef = useRef<HTMLElement>(null)
+  const [year, setYear] = useState(2024)
+  const vw = useVW()
+
+  // Choropleth using Observable Plot
   useEffect(() => {
     const marketSharesJSON = d3.csvParse(marketSharesCSV)
-    const marketShare2018 = marketSharesJSON.filter(county => +county.year === 2018)
-    // const marketShare2018Array = 
-    const marketShare2018Map = new Map(marketShare2018.map(({cfips, market_share}) => [cfips, +market_share]))
+    const currentMarketShare = marketSharesJSON.filter(county => +county.year === (year - 6))
+    const currentMarketShareMap = new Map(currentMarketShare.map(({cfips, market_share}) => [cfips, +market_share]))
 
     // @ts-expect-error because vite imports files as strings
     const counties = topojson.feature(usaTopology, usaTopology.objects.counties)
@@ -120,7 +157,7 @@ const DataStory = () => {
       projection: "albers-usa",
       marks: [
         Plot.geo(counties, {
-          fill: (d) => marketShare2018Map.get(d.id),
+          fill: (d) => currentMarketShareMap.get(d.id),
         }),
         Plot.geo(states, {
           stroke: "#f8fafc", // slate-50
@@ -142,44 +179,36 @@ const DataStory = () => {
     return () => {
       map.remove()
     }
-  }, [vw])
+  }, [vw, year])
 
   return (
-    <section className="sm:grid grid-rows-[min-content_min-content_1fr_1fr] grid-cols-3">
-      <article className="row-span-2 col-span-2 bg-zinc-100 text-zinc-800 p-2">
-        <h2 className="text-5xl font-black uppercase">Here are the <b className="text-blue-800">facts</b></h2>
-        <div className="grid lg:grid-cols-[75%_25%] xl:grid-cols-[640px_1fr]">
-          <figure ref={customerGrowthGraphRef}></figure>
-          <div className="flex flex-col justify-center p-4">
-            <h3 className="text-lg capitalize underline">Unprecedented customer <b>growth</b></h3>
-            <p>Our customer base has surged from zero to 25,000 in just 20 years. Join thousands of satisfied shoppers and experience why GoKart is the go-to destination for all your needs.</p>
-          </div>
-        </div>
-      </article>
-
-      <article className="col-start-3 row-span-3 p-4 bg-blue-950">
-        <h3 className="text-zinc-50 font-bold text-xl capitalize text-center">A wealth of product categories</h3>
-        <div className="flex flex-wrap justify-center gap-x-4">
-          <figure className="relative basis-[max(30vw,256px)] my-8">
-            <div id="category-pie-chart"></div>
-            <div className="absolute inset-0 grid justify-center items-center">
-              <caption id="category-pie-chart-legend" className="grid grid-cols-[min-content_1fr] gap-x-1 justify-center text-zinc-400 text-left"></caption>
-            </div>
-          </figure>
-          <div className="flex items-center basis-64 grow">
-            <p className="text-zinc-200 text-center">Understand the variety of products we offer by exploring the distribution of items across different categories. This breakdown highlights our diverse inventory, ensuring something for everyone.</p>
-          </div>
-        </div>
-      </article>
-
-      <article className="col-start-1 col-span-2 row-start-3 row-span-2 p-2 bg-slate-800">
-        <h3 className="text-3xl text-center text-slate-100 font-bold capitalize">Nation-wide adoption</h3>
-        <figure ref={choroplethRef} className="flex justify-center"></figure>
-      </article>
-
-      <article className="col-start-3 row-start-4 bg-zinc-900"></article>
-    </section>
+    <article className="col-start-1 col-span-2 row-start-3 row-span-2 p-2 bg-slate-800">
+      <h3 className="text-3xl text-center text-slate-100 font-bold capitalize">Nation-wide adoption</h3>
+      <figure ref={choroplethRef} className="relative grid justify-center m-2 shadow-lg shadow-slate-900">
+        <label className="sm:absolute top-0 right-0 flex justify-center bg-gradient-to-b from-slate-600 to-slate-800 p-2 gap-2 shadow-sm shadow-slate-900 text-xs lg:text-base">
+          <input type="range" min={2006} max={2024} value={year} onChange={(e) => setYear(+e.target.value)} className="sm:max-w-[14vw] lg:w-64" />
+          <h4 className="bg-gradient-to-b from-slate-500 to-slate-600 shadow-sm shadow-slate-700 p-1 rounded font-bold text-slate-300 w-12 text-center">{year}</h4>
+        </label>
+        <caption className="text-slate-600 order-1 my-2">States that are not shaded were not included in the survey.</caption>
+      </figure>
+      <p className="text-center text-slate-200 my-4">Explore our nationwide customer base with this interactive choropleth map, showcasing GoKart's reach across the United States. See how our products have been widely adopted from coast to coast, reflecting our growth and popularity.</p>
+    </article>
   )
 }
+
+const TotalSales = () => {
+  return <article className="col-start-3 row-start-4 bg-black flex flex-wrap items-center p-4 gap-1">
+    <h4 className="font-black text-white text-9xl underline">1M</h4>
+    <p className="text-slate-200">Sales since we were founded.</p>
+  </article>
+}
+
+const DataStory = () => 
+  <section className="sm:grid grid-rows-[min-content_min-content_1fr_1fr] grid-cols-3">
+    <CustomerGrowth />
+    <CategoryDistribution />
+    <MarketShareChoropleth />
+    <TotalSales />
+  </section>
 
 export default DataStory
