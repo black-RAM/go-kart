de0fb2599e52f5f417a99c4eec0505f1f58d63b8
black-RAM <pplanes311@gmail.com>
2024-08-05 22:25:26 +0300

    checkout page more mobile-friendly

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -7,6 +7,7 @@ import woosh from "../assets/fast-whoosh-118248.mp3"
 import rev from "../assets/revving.mp3"
 import usePlayer from "../hooks/usePlayer"
 import Product from "../types/Product"
+import processing from "../assets/computer-processing-sound-effects-short-click-select-01-122134.mp3"
 
 interface CountedProduct extends Product {
   count: number,
@@ -17,6 +18,7 @@ const Checkout = () => {
   const {pathname} = useLocation()
   const [playWoosh] = usePlayer(woosh)
   const [playRev] = usePlayer(rev)
+  const [playProcessing] = usePlayer(processing)
   const {cart, catalog} = useContext(ShopContext)
   const products = Object.entries(cart)
     .filter(entry => entry[1] > 0)
@@ -30,12 +32,17 @@ const Checkout = () => {
     const sub = product.subtotal
     return sum + sub
   }, 0)
+
+  const disappointUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    alert("Happy April Fools! This was all a joke to show off my front-end web development skills!")
+  }
   
   return (
     <main className="checkout-background bg-cover bg-center bg-fixed" style={{backgroundImage: `url(${wallpaper})`}}>
       {
         products.length > 0 ? 
-        <div className="grid grid-rows-[repeat(2,max-content)] lg:grid-cols-2 lg:grid-rows-1 items-start py-4 px-8 gap-8">
+        <div className="grid grid-rows-[repeat(2,max-content)] lg:grid-cols-2 lg:grid-rows-1 items-start py-2 px-2 sm:py-4 sm:px-8 gap-8">
           <section className="checkout-receipt">
             <hgroup className="flex items-end justify-center border-b-2 border-b-slate-800 pb-2 gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className="size-8 fill-slate-600">
@@ -44,7 +51,7 @@ const Checkout = () => {
               <h1 className="text-slate-600 text-3xl font-bold">Receipt</h1>
             </hgroup>
             <table className="w-full">
-              <thead className="text-slate-400">
+              <thead className="text-slate-400 text-xs sm:text-sm">
                 <tr>
                   <th>Image</th>
                   <th>Product</th>
@@ -53,7 +60,7 @@ const Checkout = () => {
                   <th>Subtotal</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="text-sm sm:text-base">
                 {products.map((product, index) => 
                   <tr key={index} className="border-b border-b-blue-100 shadow">
                     <td>
@@ -88,7 +95,7 @@ const Checkout = () => {
               <h2 className="text-blue-200 text-3xl font-bold">Payment</h2>
             </hgroup>
 
-            <form className="grid grid-cols-2 gap-x-4 gap-y-2">
+            <form onSubmit={disappointUser} className="grid grid-cols-2 gap-x-4 gap-y-2">
               <h3 className="form-section-header">Contact</h3>
               <div>
                 <label htmlFor="first-name">First Name</label>
@@ -132,7 +139,7 @@ const Checkout = () => {
               
               <label htmlFor="notes" className="form-section-header">Additional Notes</label>
               <textarea id="notes" name="notes" className="col-span-2"></textarea>
-              <button type="submit" className="col-span-2 p-2 rounded bg-gradient-to-r from-green-400 to-green-600 shadow shadow-black hover:shadow-md hover:shadow-black tracking-widest flex gap-1 justify-center focus:border-none focus:outline-none focus:outline-blue-200">
+              <button type="submit" onClick={playProcessing} className="col-span-2 p-2 rounded bg-gradient-to-r from-green-400 to-green-600 shadow shadow-black hover:shadow-md hover:shadow-black tracking-widest flex gap-1 justify-center focus:border-none focus:outline-none focus:outline-blue-200">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className="size-6 fill-black">
                   <path d="M475-160q4 0 8-2t6-4l328-328q12-12 17.5-27t5.5-30q0-16-5.5-30.5T817-607L647-777q-11-12-25.5-17.5T591-800q-15 0-30 5.5T534-777l-11 11 74 75q15 14 22 32t7 38q0 42-28.5 70.5T527-522q-20 0-38.5-7T456-550l-75-74-175 175q-3 3-4.5 6.5T200-435q0 8 6 14.5t14 6.5q4 0 8-2t6-4l136-136 56 56-135 136q-3 3-4.5 6.5T285-350q0 8 6 14t14 6q4 0 8-2t6-4l136-135 56 56-135 136q-3 2-4.5 6t-1.5 8q0 8 6 14t14 6q4 0 7.5-1.5t6.5-4.5l136-135 56 56-136 136q-3 3-4.5 6.5T454-180q0 8 6.5 14t14.5 6Zm-1 80q-37 0-65.5-24.5T375-166q-34-5-57-28t-28-57q-34-5-56.5-28.5T206-336q-38-5-62-33t-24-66q0-20 7.5-38.5T149-506l232-231 131 131q2 3 6 4.5t8 1.5q9 0 15-5.5t6-14.5q0-4-1.5-8t-4.5-6L398-777q-11-12-25.5-17.5T342-800q-15 0-30 5.5T285-777L144-635q-9 9-15 21t-8 24q-2 12 0 24.5t8 23.5l-58 58q-17-23-25-50.5T40-590q2-28 14-54.5T87-692l141-141q24-23 53.5-35t60.5-12q31 0 60.5 12t52.5 35l11 11 11-11q24-23 53.5-35t60.5-12q31 0 60.5 12t52.5 35l169 169q23 23 35 53t12 61q0 31-12 60.5T873-437L545-110q-14 14-32.5 22T474-80Zm-99-560Z"/>
                 </svg>
