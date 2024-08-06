8bc6cbd4b82f4252854eb385c4fa438e9a89bd5b
black-RAM <pplanes311@gmail.com>
2024-08-06 18:48:26 +0300

    redirect back to root from Error page

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,4 +1,5 @@
-import { Link, useRouteError } from "react-router-dom"
+import { useEffect, useRef, useState } from "react"
+import { Link, useNavigate, useRouteError } from "react-router-dom"
 import wallpaper from "../assets/ivan-bonadeo-qa1QZOFPmdI-unsplash.jpg"
 import "../styles/404.css"
 
@@ -8,6 +9,22 @@ interface ErrorObject {
 
 const Error404 = () => {
   const error = useRouteError() as ErrorObject
+  const [secondsToRedirect, setSecondsToRedirect] = useState(10)
+  const countdownRef = useRef<number | null>(null)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    countdownRef.current = setInterval(() => {
+      setSecondsToRedirect(s => Math.max(s - 1, 0))
+    }, 1000)
+    return () => {
+      clearInterval(countdownRef.current as number)
+    }
+  }, [])
+
+  useEffect(() => {
+    if(secondsToRedirect <= 0) navigate("/")
+  }, [secondsToRedirect, navigate])
 
   return (
     <main className="h-dvh bg-center bg-cover p-8" id="error-element" style={{backgroundImage: `url(${wallpaper})`}}>
@@ -16,6 +33,7 @@ const Error404 = () => {
       <div className="backdrop-blur max-w-max p-4 rounded grid gap-4 bg-[#9ca3af80] sm:bg-transparent">
         <p className="text-gray-200 sm:text-gray-600 text-2xl sm:text-4xl">Looks like you've gone off-track.</p>
         <Link to="/">Get back in the race!</Link>
+        <p className="font-mono text-xl text-gray-400">Redirect in {secondsToRedirect}s</p>
       </div>
     </main>
   )
